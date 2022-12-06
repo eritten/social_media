27ca8648d64dd6dd4e24132fe0223c15c5c114d5
eritten <eritten2@gmail.com>
2022-12-06 09:33:48 +0000

    adding media files

--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 from config import db
+from datetime import datetime
 
 class ChatUser(db.Document):
     username = db.StringField()
@@ -6,3 +7,26 @@ class ChatUser(db.Document):
     telephone_number = db.StringField()
     password = db.StringField()
     is_login = db.BooleanField(default=False)
+    last_login = db.DateTimeField(default=datetime.now())
+    def __str__(self):
+        return self.username
+
+    images = db.ReferenceField('Image')
+
+
+class Image(db.EmbeddedDocument):
+    url = db.URLField()
+    property_id = db.ReferenceField('Property')
+
+
+
+class Property(db.Document):
+    title = db.StringField()
+    description = db.StringField()
+    image = db.EmbeddedDocumentListField('Image')
+
+    date = db.DateTimeField(default=datetime.now())
+    price = db.DecimalField()
+    tags = db.ListField()
+    def __str__(self):
+        return self.title
